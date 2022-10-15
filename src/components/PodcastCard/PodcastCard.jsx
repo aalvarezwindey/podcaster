95184f08b50af2fe64d8f65e40c0f40aeabd9865
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-10-14 21:41:13 -0300

    fix: fix some styles

--- src/components/PodcastCard/PodcastCard.jsx
+++ src/components/PodcastCard/PodcastCard.jsx
@@ -5,7 +5,7 @@ export default function PodcastCard({ name, author, imageURL }) {
   return (
     <article className={styles.card}>
       <img alt={`${name} - ${author}`} src={imageURL} />
-      <h2>{name}</h2>
+      <h2 title={name}>{name}</h2>
       <p>Author: {author}</p>
     </article>
   );
