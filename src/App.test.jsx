88067d576a5808e79854af6af6bdd3a7cc24cb08
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-10-14 21:47:50 -0300

    feat: add acceptance criteria TODO tests

---
+++ src/App.test.jsx
@@ -0,0 +1,13 @@
+describe('Acceptance criteria', () => {
+  it.todo('should list podcasts in main page');
+
+  it.todo('should filter podcasts based on filter input');
+
+  it.todo('should navigate to podcast detail page when a podcast is selected');
+
+  it.todo('should list podcast episodes in podcast detail page');
+
+  it.todo(
+    'should navigate to podcast episode detail page when a podcast episode is selected'
+  );
+});
