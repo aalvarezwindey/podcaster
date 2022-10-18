2139d79f7167e18c3bff47f87b6e0d8cda0693ab
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-10-17 21:03:37 -0300

    feat: add cs navigation animation

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useLayoutEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './AppHeader.module.css';
 
+const NAVIGATION_ANIMATION_DURATION = 500;
+
 export default function AppHeader() {
+  const [
+    showClientSideNavigationAnimation,
+    setShowClientSideNavigationAnimation,
+  ] = useState(false);
+  const location = useLocation();
+  useLayoutEffect(() => {
+    setShowClientSideNavigationAnimation(true);
+    const timer = setTimeout(() => {
+      setShowClientSideNavigationAnimation(false);
+    }, NAVIGATION_ANIMATION_DURATION);
+    return () => clearTimeout(timer);
+  }, [location.pathname]);
   return (
     <header className={styles.header}>
       <h1>
         <Link to="/">Podcaster</Link>
       </h1>
+      {showClientSideNavigationAnimation ? <div /> : null}
     </header>
   );
 }
